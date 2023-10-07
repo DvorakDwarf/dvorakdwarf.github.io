fdc94d2aa9517b47325f7e59f75f7b4cc3b0fb13
HistidineDwarf <dvorakdwarf@gmail.com>
2023-10-06 18:06:38 -0700

    Dupe

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -40,11 +40,11 @@ function Home() {
           </div>
         </ErrorBoundary>
       </Suspense>
-      <div className="extra-credits credits">      
+      <div className="extra-credits">      
         <span>Color theme is <a href='https://github.com/SweetPastel'>SweetPastel</a></span>
         <span>Art by <a href='https://twitter.com/noellemonade'>@noellemonade</a></span>
         <span>Hard carried by <a href='https://github.com/zeromero-dev'>zeromero</a></span>
-    </div>
+      </div>
     </div>
     <Footer />
     </div>
